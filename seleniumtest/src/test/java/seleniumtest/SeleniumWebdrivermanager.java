89bd12f84703fdb96ae256d4daf5dae276a7b2c2
7J4333897 <7J4333897@DESKTOP-CB0R8GJ>
2022-11-14 07:27:44 -0500

    commit on 14/14

---
+++ seleniumtest/src/test/java/seleniumtest/SeleniumWebdrivermanager.java
@@ -0,0 +1,21 @@
+package seleniumtest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SeleniumWebdrivermanager {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("Http://google.com");
+		driver.close();
+		
+	}
+
+}
